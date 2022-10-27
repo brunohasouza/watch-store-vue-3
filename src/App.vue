b924066a3f4d9c0e6ec1125f25fc83634af609e5
Bruno Souza <souza.brunoha@gmail.com>
2022-10-26 23:57:37 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex w-full h-screen bg-emerald-500 items-center justify-center">
+    <h1 class="text-center font-semibold text-4xl text-white">Hellow World</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
